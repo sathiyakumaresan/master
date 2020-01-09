80dfb6f2059392956d66dbb0f279e24b7fcbec13
sathiyakumaresan <59592404+sathiyakumaresan@users.noreply.github.com>
2020-01-09 15:20:53 +0530

    Update arrayvaluesupper.php

--- php/09-Jan/array/arrayvaluesupper.php
+++ php/09-Jan/array/arrayvaluesupper.php
@@ -1,4 +1,9 @@
 <?php
-$Color = array('A' => 'Blue', 'B' => 'Green', 'c' => 'Red');
+$color = array('A' => 'Blue', 'B' => 'Green', 'c' => 'Red');
+$color=array_change_key_case($color,CASE_LOWER);
+$color=array_flip($color);
+$color=array_change_key_case($color,CASE_UPPER);
+$color=array_flip($color);
+$color=array_change_key_case($color,CASE_UPPER);
 print_r($color);
 ?>
